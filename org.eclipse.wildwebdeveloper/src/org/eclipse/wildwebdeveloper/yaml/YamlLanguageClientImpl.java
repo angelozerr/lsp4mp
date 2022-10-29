016708c056aca67721cef16dbfc99b6e18f915b4
azerr <azerr@redhat.com>
2022-10-29 08:42:09 +0200

    YAML preferences
    Signed-off-by: azerr <azerr@redhat.com>

--- org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YamlLanguageClientImpl.java
+++ org.eclipse.wildwebdeveloper/src/org/eclipse/wildwebdeveloper/yaml/YamlLanguageClientImpl.java
@@ -9,16 +9,37 @@
  *******************************************************************************/
 package org.eclipse.wildwebdeveloper.yaml;
 
+import static org.eclipse.wildwebdeveloper.yaml.ui.preferences.YAMLPreferenceServerConstants.isMatchYamlSection;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4e.LanguageClientImpl;
+import org.eclipse.lsp4j.ConfigurationItem;
 import org.eclipse.lsp4j.ConfigurationParams;
+import org.eclipse.wildwebdeveloper.ui.preferences.Settings;
+import org.eclipse.wildwebdeveloper.yaml.ui.preferences.YAMLPreferenceServerConstants;
 
 public class YamlLanguageClientImpl extends LanguageClientImpl {
 
 	@Override
-	public CompletableFuture<List<Object>> configuration(ConfigurationParams configurationParams) {
-		return CompletableFuture.completedFuture(List.of(YAMLLanguageServer.getYamlConfigurationOptions()));
+	public CompletableFuture<List<Object>> configuration(ConfigurationParams params) {
+		return CompletableFuture.supplyAsync(() -> {
+			List<Object> settings = new ArrayList<>();
+			for (ConfigurationItem item : params.getItems()) {
+				String section = item.getSection();
+				if (isMatchYamlSection(section)) {
+					// 'yaml' section, returns the yaml settings
+					Settings yamlSettings = YAMLPreferenceServerConstants.getGlobalSettings();
+					settings.add(yamlSettings.findSettings(section.split("[.]")));
+				} else {
+					// Unkwown section
+					settings.add(null);
+				}
+			}
+			return settings;
+		});
 	}
+
 }
